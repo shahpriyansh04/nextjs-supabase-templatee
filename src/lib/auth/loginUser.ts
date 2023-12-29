e980fdfdb27833a9752898604c26d75499d46310
Priyansh Shah <shah.priyansh04@gmail.com>
2023-12-30 00:52:08 +0530

    3

--- src/lib/auth/loginUser.ts
+++ src/lib/auth/loginUser.ts
@@ -1,7 +1,7 @@
-import { AuthError, AuthResponse, OAuthResponse, User } from "@supabase/supabase-js";
+import {  AuthResponsePassword } from "@supabase/supabase-js";
 import supabase from "../supabase";
 
-export default async function loginUser(email:string, password:string): Promise<AuthResponse> {
+export default async function loginUser(email:string, password:string): Promise<AuthResponsePassword> {
   const  data = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
